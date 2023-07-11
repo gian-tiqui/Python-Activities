a2791d8fab11a65b210226b82cb822ef05cebda0
gian <michaelgiantiqui3@gmail.com>
2023-07-11 19:56:37 +0800

    meow

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from DSA.binary_tree import Node, insert, in_order
 
 if __name__ == '__main__':
 
-    root = Node(5)
+    root: Node = Node(5)
+    num: int = 3
+    num2: int = 7
 
-    insert(root, 3)
-    insert(root, 7)
+    insert(root, num)
+    insert(root, num2)
 
     in_order(root)
